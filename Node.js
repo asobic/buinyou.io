76a9fedde7df53c8df2a0f42550aab3472bc47b1
Naoo122 <kingfishernao2017@gmail.com>
2025-10-08 07:30:29 +0900

    Node.js の更新

--- Node.js
+++ Node.js
@@ -37,13 +37,16 @@
     Data.hidden = true;
     GamePost.hidden = false;
 }
-
-  window.FormEntered = function()｛
-    var name = document.getElementById("name")
-    var creator = document.getElementById("creator")
-    var gameDiscription = document.getElementById("gameDiscription")
-    var gameURL = document.getElementById("gameURL")
-    const element =  document.getElementById("basic")
-    element.insertAdjacentHTML("afterend","<p>作品名：${name.value}<br>制作者名：${creator.value}<br>ロゴ画像：<br>作品URL：${gameURL.value}")
+window.FormEntered = function() {
+  var name = document.getElementById("name");
+  var creator = document.getElementById("creator");
+  var gameDiscription = document.getElementById("gameDiscription");
+  var gameURL = document.getElementById("gameURL");
+  const element = document.getElementById("basic");
+  element.insertAdjacentHTML(
+    "afterend",
+    `<p>作品名：${name ? name.value : ""}<br>制作者名：${creator ? creator.value : ""}<br>ロゴ画像：<br>作品URL：${gameURL ? gameURL.value : ""}</p>`
+  );
+};
 ｝
 
